2cc88c906b21de2b1a0fbed639ef1dc12da45c08
telmo <telmomcalheiros@gmail.com>
2026-03-25 17:25:48 -0300

    Tratamento de arquivos e parametros inválidos

--- source/main_edf.c
+++ source/main_edf.c
@@ -5,7 +5,16 @@
 
 
 int main(int argc, char *argv[]){
+    if (argc > 2){
+        printf("ERRO! Certifique-se de passar apenas dois parâmetros!\n");
+        return 0;
+    }
     FILE *arquivo = fopen(argv[1], "r");
+
+    if (arquivo == NULL){
+        printf("ERRO! Certifique-se de passar um arquivo existente\n");
+        return 0;
+    }
     edf(arquivo);
 
     fclose(arquivo);
